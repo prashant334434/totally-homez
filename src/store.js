77aabd2ceca5a1442b4a62715558a55f04e5cc9a
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2023-04-27 22:04:42 +0530

    redux setup done

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { createStore,combineReducers,applyMiddleware} from "redux"
+import thunk from "redux-thunk"
+import {composeWithDevTools} from 'redux-devtools-extension'
+import { userReducer } from "./reducers/userReducers";
+
+const reducer=combineReducers({
+    user:userReducer,
+    
+   
+
+
+
+})
+let initailState={};
+const middleware=[thunk]
+const store=createStore(reducer,initailState,composeWithDevTools(applyMiddleware(...middleware))
+)
+export default store
