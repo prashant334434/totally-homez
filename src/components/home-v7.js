f7b6193e7fa09b7ea62fa206db2cfac62f147b97
prashant334434 <prashantvashisth53@gamil.com>
2023-04-27 13:53:38 +0530

    first commit

---
+++ src/components/home-v7.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Navbar from './global-components/navbar-v4';
+import BannerV6 from './section-components/banner-v6';
+import Aboutv2 from './section-components/about-v2';
+import Featuresv1 from './section-components/features-v1';
+import ProSlider from './section-components/product-slider-v4';
+import BlogSlider from './blog-components/blog-slider-v1';
+import Gallery from './section-components/gallery-v1';
+import Apartment from './section-components/apartment-v1';
+import VideoV1 from './section-components/video-v1';
+import Category from './section-components/category-v1';
+import Testimonial from './section-components/testimonial-v1';
+import CallToActionV1 from './section-components/call-to-action-v1';
+import Footer from './global-components/footer';
+import BestProper from './section-components/BestProperty';
+import BestProperty from './section-components/BestProperty';
+import BestVilla from './section-components/BestVilla';
+import Villa from './section-components/Villa';
+import BestApartment from './section-components/BestApartment';
+import Apartments from './section-components/Apartment';
+
+
+const Home_V7 = () => {
+    return <div>
+        <Navbar CustomClass="ltn__header-transparent gradient-color-2" /> 
+        <BannerV6 />
+        <BestProperty/>
+        {/* <Aboutv2 /> */}
+        <Gallery />
+        <BestVilla />
+        <Villa />
+        <BestApartment />
+        <Apartments />
+        <Featuresv1 customClass="ltn__feature-area section-bg-1 pt-120 pb-90 mb-120---"/>
+        <ProSlider />
+        <Gallery />
+        <Apartment CustomClass="pt-115" />
+        <VideoV1 />
+        <Category />
+        <Testimonial />
+        <BlogSlider customClass="section-subtitle-2"/>
+        <CallToActionV1 />
+        <Footer />
+    </div>
+}
+
+export default Home_V7
+
