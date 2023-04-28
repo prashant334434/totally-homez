429626cf9579214c2a281074c7a4f2d554b7a830
prashant334434 <prashantvashisth53@gamil.com>
2023-04-28 15:58:23 +0530

    get a counsal

--- src/components/section-components/call-to-action-v1.js
+++ src/components/section-components/call-to-action-v1.js
@@ -19,7 +19,7 @@ class CallToActonV1 extends Component {
 			            <p>We can help you realize your dream of a new home</p>
 			          </div>
 			          <div className="btn-wrapper go-top">
-			            <Link className="btn btn-effect-3 btn-white" to="/contact">Explore Properties <i className="icon-next" /></Link>
+			            <Link className="btn btn-effect-3 btn-white" to="/contact">Get A Consultancy <i className="icon-next" /></Link>
 			          </div>
 			        </div>
 			      </div>
