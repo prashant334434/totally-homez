09b10ddd57d79ffc60004f78830621b9e6c2f2b1
prashant334434 <prashantvashisth53@gamil.com>
2023-05-09 13:05:43 +0530

    header

--- src/components/section-components/OffPlan.js
+++ src/components/section-components/OffPlan.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Navbar from '../global-components/navbar';
+// import Navbar from './global-components/navbar-v4';
+// import Navbar from './global-components/navbar-v4';
 import PageHeader from '../global-components/page-header';
 import ShogGrid from '../shop-components/shop-grid-v1';
 import CallToActionV1 from '../section-components/call-to-action-v1';
@@ -14,13 +15,16 @@ import AboutOffPlan from './AboutOffPlan';
 import CounterFeatures from './CounterFeatures';
 import Carousel from './carousel/Carousel';
 import { IMAGES, VIDEOS, LARGE_IMAGES } from "../../data/data";
+import NavbarV3 from '../global-components/navbar-v4';
+import Footer2 from '../global-components/Footer2';
 
 const OffPlan = () => {
   return ( 
   
   <div>
      <div>
-        <Navbar page={"offplan"} />
+     <NavbarV3 CustomClass="ltn__header-transparent gradient-color-2" /> 
+
         <AboutOffPlan/>
         <div className='container'>
       </div>
@@ -37,6 +41,7 @@ const OffPlan = () => {
 
         <ApartmentV3/>
         <CallToActionV1 />
+        <Footer2 customClass='ltn__feature-area section-bg-1 pt-120 pb-90 mb-120---'/>
         <Footer_v1 />
     </div>
   </div>
