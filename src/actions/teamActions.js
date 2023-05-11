36dd5009ce6f96eaa2ae3fce18a5d9dabc33e3f0
prashant334434 <prashantvashisth53@gamil.com>
2023-05-11 12:54:55 +0530

    tea api data rendered

--- src/actions/teamActions.js
+++ src/actions/teamActions.js
@@ -6,7 +6,9 @@ export const getTeamsApi = () => async (dispatch) => {
     try {
       dispatch({ type: GET_ALL_TEAM_REQUEST });
   
-      const  data  = await axios.get(`${API_URL}/teams`);
+      const  data  = await axios.get(`https://totallyhomerealestate.com/API/api/teams`);
+
+      console.log("action",data)
   
       dispatch({
         type: GET_ALL_TEAM_SUCCESS,
