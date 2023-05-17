d7e52da6f87eab7354f34d5bdbb364c0ebb57d84
prashant334434 <prashantvashisth53@gamil.com>
2023-05-17 18:21:52 +0530

    off plan done

--- src/components/section-components/OffPlan.js
+++ src/components/section-components/OffPlan.js
@@ -23,6 +23,7 @@ import PaymentPlan from './PaymentPlan';
 import BannerV2 from './banner-v2';
 import TestimonialV2 from './testimonial-v2';
 import Neighbour from './neighbour';
+import ApartmentV2 from './apartment-v2';
 const OffPlan = () => {
   return ( 
   
@@ -44,6 +45,7 @@ const OffPlan = () => {
         <BlogRightSidebarPage/>
         <CounterV1 customClass="ltn__feature-area section-bg-1"/>
         <ApartmentV3/>
+        {/* <ApartmentV2/> */}
         <TestimonialV2 />
         {/* <Neighbour /> */}
 
