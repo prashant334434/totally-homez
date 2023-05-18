6fc3623ab1413d0d8c45b544baf7a23c0fd097a8
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2023-05-18 15:54:27 +0530

    nav done

--- src/components/global-components/Mobile-nav.js
+++ src/components/global-components/Mobile-nav.js
@@ -65,116 +65,38 @@ class MobileNav extends Component {
 					<div className="ltn__utilize-menu">
 					<ul>
 						<li><a href="#">Home</a>
+				
+						</li>
+						<li><Link to="/about">Buy</Link>
 						<ul className="sub-menu">
-						<li><Link to="/">Home Style 01</Link></li>
-						<li><Link to="/home-v2">Home Style 02</Link></li>
-						<li><Link to="/home-v3">Home Style 03</Link></li>
-						<li><Link to="/home-v4">Home Style 04</Link></li>
-						<li><Link to="/home-v5">Home Style 05 <span className="menu-item-badge">video</span></Link></li>
-						<li><Link to="/home-v6">Home Style 06</Link></li>
-						<li><Link to="/home-v7">Home Style 07</Link></li>
-						<li><Link to="/home-v8">Home Style 08</Link></li>
-						<li><Link to="/home-v9">Home Style 09</Link></li>
-						<li><Link to="/home-v10">Home Style 10</Link></li>
+							<li><Link to="/about">Apartments</Link></li>
+							<li><Link to="/service">Villas</Link></li>
+							<li><Link to="/service-details">Penthouse</Link></li>
+							<li><Link to="/portfolio">Townhouse</Link></li>
+							<li><Link to="/portfolio-v2"> Off Plan</Link></li>
+						
 						</ul>
 						</li>
-						<li><Link to="/about">About</Link>
+						<li><Link to="/shop">Rent</Link>
 						<ul className="sub-menu">
-							<li><Link to="/about">About</Link></li>
-							<li><Link to="/service">Services</Link></li>
-							<li><Link to="/service-details">Service Details</Link></li>
-							<li><Link to="/portfolio">Portfolio</Link></li>
-							<li><Link to="/portfolio-v2">Portfolio - 02</Link></li>
-							<li><Link to="/portfolio-details">Portfolio Details</Link></li>
-							<li><Link to="/team">Team</Link></li>
-							<li><Link to="/team-details">Team Details</Link></li>
-							<li><Link to="/faq">FAQ</Link></li>
-							<li><Link to="/location">Google Map Locations</Link></li>
-						</ul>
+							<li><Link to="/about">Apartments</Link></li>
+							<li><Link to="/service">Villas</Link></li>
+							<li><Link to="/service-details">Penthouse</Link></li>
+							<li><Link to="/portfolio">Townhouse</Link></li>
+						
+						</ul></li>
+						<li><Link to="/blog-grid">Off Plan</Link>
+					
 						</li>
-						<li><Link to="/shop">Shop</Link>
-						<ul className="sub-menu">
-							<li><Link to="/shop">Shop</Link></li>
-							<li><Link to="/shop-grid">Shop Grid</Link></li>
-							<li><Link to="/shop-left-sidebar">Shop Left sidebar</Link></li>
-							<li><Link to="/shop-right-sidebar">Shop Right sidebar</Link></li>
-							<li><Link to="/product-details">Shop Details</Link></li>
-							<li><Link to="/cart">Cart</Link></li>
-							<li><Link to="/checkout">Checkout</Link></li>
-							<li><Link to="/my-account">My Account</Link></li>
-							<li><Link to="/login">Sign in</Link></li>
-							<li><Link to="/register">Register</Link></li>
-						</ul>
-						</li>
-						<li><Link to="/blog-grid">News</Link>
-						<ul className="sub-menu">
-							<li><Link to="/blog">News</Link></li>
-							<li><Link to="/blog-grid">News Grid</Link></li>
-							<li><Link to="/blog-left-sidebar">News Left sidebar</Link></li>
-							<li><Link to="/blog-right-sidebar">News Right sidebar</Link></li>
-							<li><Link to="/blog-details">News details</Link></li>
-						</ul>
-						</li>
-						<li><Link to="#">Pages</Link>
-							<ul class="sub-menu">
-								<li><Link to="/about">About</Link></li>
-								<li><Link to="/service">Services</Link></li>
-								<li><Link to="/service-details">Service Details</Link></li>
-								<li><Link to="/portfolio">Portfolio</Link></li>
-								<li><Link to="/portfolio-2">Portfolio - 02</Link></li>
-								<li><Link to="/portfolio-details">Portfolio Details</Link></li>
-								<li><Link to="/team">Team</Link></li>
-								<li><Link to="/team-details">Team Details</Link></li>
-								<li><Link to="/faq">FAQ</Link></li>
-								<li><Link to="/history">History</Link></li>
-								<li><Link to="/add-listing">Add Listing</Link></li>
-								<li><Link to="/locations">Google Map Locations</Link></li>
-								<li><Link to="/404">404</Link></li>
-								<li><Link to="/contact">Contact</Link></li>
-								<li><Link to="/coming-soon">Coming Soon</Link></li>
-							</ul>
+						<li><Link to="#">Exclusive Properties</Link>
+						
                     	</li>
+						<li><Link to="/contact-us">Explore Ducai</Link></li>
+						<li><Link to="/contact-us">Our Mission</Link></li>
 						<li><Link to="/contact-us">Contact</Link></li>
 					</ul>
 					</div>
-					<div className="ltn__utilize-buttons ltn__utilize-buttons-2">
-					<ul>
-						<li>
-						<Link to="/my-account" title="My Account">
-							<span className="utilize-btn-icon">
-							<i className="far fa-user" />
-							</span>
-							My Account
-						</Link>
-						</li>
-						<li>
-						<Link to="/wishlist" title="Wishlist">
-							<span className="utilize-btn-icon">
-							<i className="far fa-heart" />
-							<sup>3</sup>
-							</span>
-							Wishlist
-						</Link>
-						</li>
-						<li>
-						<Link to="/cart" title="Shoping Cart">
-							<span className="utilize-btn-icon">
-							<i className="fas fa-shopping-cart" />
-							<sup>5</sup>
-							</span>
-							Shoping Cart
-						</Link>
-						</li>
-					</ul>
-					</div>
-					<div className="ltn__social-media-2">
-					<ul>
-						<li><a href="#" title="Facebook"><i className="fab fa-facebook-f" /></a></li>
-						<li><a href="#" title="Twitter"><i className="fab fa-twitter" /></a></li>
-						<li><a href="#" title="Linkedin"><i className="fab fa-linkedin" /></a></li>
-						<li><a href="#" title="Instagram"><i className="fab fa-instagram" /></a></li>
-					</ul>
-					</div>
+			
 				</div>
 				</div>
 		</div>
