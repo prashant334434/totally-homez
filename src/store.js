202b286fc83b6f9f7599ac02f7d5f7563c8d3142
rohit0101rm <rohitrai.dev@gmail.com>
2023-05-25 04:19:15 +0530

    actions/reducers/constants setup done

--- src/store.js
+++ src/store.js
@@ -8,6 +8,10 @@ import { teamDetailsReducer, teamReducer } from "./reducers/teamReducers";
 import { apartmentCategoryReducer, apartmentCategoryRentReducer, catgoryPropertiesRentReducer, catgoryReducer, catgoryRentReducer, penthouseCategoryReducer, penthouseCategoryRentReducer, townhouseCategoryReducer, townhouseCategoryRentReducer } from "./reducers/catgoryReducres";
 import { blogReducer } from "./reducers/blogReducers";
 import { propertiesByTypeReducer } from "./reducers/propertyTypeReducers";
+import { contactReducer } from "./reducers/contactReducer";
+import { listWithUsReducer } from "./reducers/listWithUsReducer";
+import { careerReducer } from "./reducers/careerReducer";
+import { aboutReducer } from "./reducers/aboutReducer";
 
 const reducer=combineReducers({
     user:userReducer,
@@ -21,6 +25,11 @@ const reducer=combineReducers({
     categoryPropertiesRent:catgoryPropertiesRentReducer,
     townhouseCategoryProperties:townhouseCategoryReducer,
     townhouseRentProperties:townhouseCategoryRentReducer,
+    contact:contactReducer,
+    listWithUs:listWithUsReducer,
+    career:careerReducer,
+    about:aboutReducer ,
+
 
     blogs:blogReducer,
 
