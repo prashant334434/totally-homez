761e3b2cba4581e473ccd64fc765186405daa1f4
prashant334434 <prashantvashisth53@gamil.com>
2023-05-25 11:05:51 +0530

    guiline apage changes

--- src/components/shop-components/GuideLine.js
+++ src/components/shop-components/GuideLine.js
@@ -3,13 +3,21 @@ import MobileNav from '../global-components/Mobile-nav'
 import Footer_v1 from '../global-components/footer'
 import CallToActionV1 from '../section-components/call-to-action-v1';
 import Cummunity from './Cummunity';
+import CommunityGrid from './CommunityGrid';
+import CarouselSlider from './Carousel';
 
 
 const GuideLine = () => {
   return (
     <div>
         <MobileNav />
+        <CommunityGrid/>
+      
+
         <Cummunity/>
+        <h1>Investment Opportunities 
+        </h1>
+        <CarouselSlider/>
         <CallToActionV1 />
         <Footer_v1 />
     </div>
