8a31b78050c2bc29418c8b0efc2fb7a5b645376e
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2023-05-25 16:45:17 +0530

    contact api in progress

--- src/components/section-components/photocontact.js
+++ src/components/section-components/photocontact.js
@@ -16,21 +16,22 @@ const PhotoContact= ({contactData}) => {
   return (
 	<div>
 		{
-			contactData?.map((contact)=>(
-				<div key={contact?.id} className="ltn__upcoming-project-area section-bg-1--- bg-image-top " data-bs-bg={`${ABOUT_IMAGE_URL}/${contact?.c_img}`}>
-				<div className="container positioning1 col-lg-12">
-				  
-				   
-					  <div className="col-lg-6">
-					 {/*   <h6 className="section-subtitle section-subtitle-2--- ltn__secondary-color--- white-color">Getting your property ready for sale is one of our first tasks! That is why we take the time to make sure your property is ready to attract buyers by giving it a fresh coat of paint if necessary and performing minor repairs or landscaping improvements. We also help you get your home ready to sell by staging it with the help of professional stagers. 
-						</h6>*/}
-  
-					  </div>
-				  </div>
-				</div>
+			contactData?.map((item,index)=>(
+				<div className="ltn__upcoming-project-area section-bg-1--- bg-image-top " data-bs-bg={`${ABOUT_IMAGE_URL}/contact}/${item?.c_img}`}>
+			  <div className="container positioning1 col-lg-12">
+			    
+			     
+			        <div className="col-lg-6">
+                   {/*   <h6 className="section-subtitle section-subtitle-2--- ltn__secondary-color--- white-color">Getting your property ready for sale is one of our first tasks! That is why we take the time to make sure your property is ready to attract buyers by giving it a fresh coat of paint if necessary and performing minor repairs or landscaping improvements. We also help you get your home ready to sell by staging it with the help of professional stagers. 
+                      </h6>*/}
+
+			        </div>
+			    </div>
+			  </div>
+
 			))
 		}
-	
+		
 			</div>
 
   )
