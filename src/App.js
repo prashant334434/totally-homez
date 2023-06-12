ab46fa755547092fa49aa8797bff59669d597524
prashangt334434 <prashantvashisth53@gmail.com>
2023-06-12 11:39:23 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -113,7 +113,7 @@ import TypePage from './components/TypePage';
         <Route path="/bread-crums" component={ BreadCrumbs } />
         <Route path="/:property_city/:property_community/:property_sub_community/:property_type-for-:property_for" component={ LevelPage } />
         <Route path="/:property_city/:property_community/:property_sub_community/:property_type-for-:property_for-:property_level" component={ LevelPage } />
-        <Route path="/sub-community-page" component={ SubCommunityPage } />
+        <Route path="/subs-community-page" component={ SubCommunityPage } />
         <Route path="/community-page" component={ CommunityPage } />
         <Route path="/type-page" component={ TypePage } />
 
