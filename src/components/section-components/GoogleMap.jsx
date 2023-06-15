b85ae47d8417b6b48e549292875d5f5e6717141b
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2023-06-15 16:12:42 +0530

    google map rendered properly

--- src/components/section-components/GoogleMap.jsx
+++ src/components/section-components/GoogleMap.jsx
@@ -4,18 +4,12 @@ import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 const GoogleMap = (props) => {
   const { latitude, longitude } = props?.propertyDetails;
   const markerOptions = {
-    position: { lat: 37.7749, lng: -122.4194 },
+    position: {
+      lat: latitude ? latitude : 25.2048, // Replace with the latitude from your map link
+      lng: longitude ? longitude : 55.2708, // Replace with the longitude from your map link
+    },
     icon: {
       url: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
-      anchor: new props.google.maps.Point(16, 16),
-      scaledSize: new props.google.maps.Size(32, 32),
-      labelOrigin: new props.google.maps.Point(16, 16),
-      label: {
-        text: '\u25CF',
-        color: 'red',
-        fontSize: '20px',
-        fontWeight: 'bold',
-      },
     },
   };
   const mapStyles = {
@@ -26,6 +20,7 @@ const GoogleMap = (props) => {
 
   return (
     <div className="mt-100 property-details-google-map mb-100 pb-90 col-lg-12">
+   
       <Map
         google={props?.google}
         zoom={14}
