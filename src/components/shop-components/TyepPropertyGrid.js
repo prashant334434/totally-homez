1d96fec3127bd08c36a5c0094a9a35da4d2b4330
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2023-06-20 10:43:43 +0530

    pagination buttons added

--- src/components/shop-components/TyepPropertyGrid.js
+++ src/components/shop-components/TyepPropertyGrid.js
@@ -8,9 +8,16 @@ import {
 } from "../../actions/catgoryActions";
 import { PROPERTY_IMAGES_URL, TEAM_API_URL } from "../../constants/config";
 import { currentTeamMember } from "../../actions/teamActions";
+import ReactPaginate from "react-paginate";
 
 const TyepPropertyGrid = (props) => {
-  console.log("TyepPropertyGrid", props);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 12;
+  const pageCount = Math.ceil(props?.propertyTypes.length / itemsPerPage);
+  const handlePageChange = (selectedPage) => {
+    setCurrentPage(selectedPage.selected);
+  };
+
   const history = useHistory();
   const dispatch = useDispatch();
   const [propertyCategory, setPropertyCategory] = useState("apartment");
@@ -26,6 +33,12 @@ const TyepPropertyGrid = (props) => {
   const url = (titleName) => {
     return titleName?.split(" ")?.join("-")?.toLowerCase();
   };
+
+  const offset = currentPage * itemsPerPage;
+  const paginatedPropertyTypes = props?.propertyTypes?.slice(
+    offset,
+    offset + itemsPerPage
+  );
   return (
     <div>
       <div>
@@ -70,83 +83,12 @@ const TyepPropertyGrid = (props) => {
               >
                 <div className="ltn__product-tab-content-inner ltn__product-grid-view">
                   <div className="row">
-                    <div className="col-lg-12">
-                      {/* Search Widget */}
-                      {/* <div className="ltn__search-widget mb-30">
-										<form action="#">
-											<input type="text" name="search" placeholder="Search your keyword..." />
-											<button type="submit"><i className="fas fa-search" /></button>
-										</form>
-										</div> */}
-                    </div>
-                    {/* ltn__product-item */}
-                    {/* <div className="col-lg-4 col-sm-6 col-12">
-										<div className="ltn__product-item ltn__product-item-4 ltn__product-item-5 text-center---">
-										<div className="product-img">
-											<Link to="/product-details"><img src={publicUrl+"assets/img/product-3/1.jpg"} alt="#" /></Link>
-											<div className="real-estate-agent">
-											<div className="agent-img">
-												<Link to="/team-details"><img src={publicUrl+"assets/img/blog/author.jpg"} alt="#" /></Link>
-											</div>
-											</div>
-										</div>
-										<div className="product-info">
-											<div className="product-badge">
-											<ul>
-												<li className="sale-badg">For Rent</li>
-											</ul>
-											</div>
-											<h2 className="product-title go-top"><Link to="/product-details">New Apartment Nice View</Link></h2>
-											<div className="product-img-location">
-											<ul>
-												<li className="go-top">
-												<Link to="/contact"><i className="flaticon-pin" /> Belmont Gardens, Chicago</Link>
-												</li>
-											</ul>
-											</div>
-											<ul className="ltn__list-item-2--- ltn__list-item-2-before--- ltn__plot-brief">
-											<li><span>3 </span>
-												Bedrooms
-											</li>
-											<li><span>2 </span>
-												Bathrooms
-											</li>
-											<li><span>3450 </span>
-												square Ft
-											</li>
-											</ul>
-											<div className="product-hover-action">
-											<ul>
-												<li>
-												<a href="#" title="Quick View" data-bs-toggle="modal" data-bs-target="#quick_view_modal">
-													<i className="flaticon-expand" />
-												</a>
-												</li>
-												<li>
-												<a href="#" title="Wishlist" data-bs-toggle="modal" data-bs-target="#liton_wishlist_modal">
-													<i className="flaticon-heart-1" /></a>
-												</li>
-												<li>
-												<span className="go-top">
-												<Link to="/product-details" title="Product Details">
-													<i className="flaticon-add" />
-												</Link>
-												</span>
-												</li>
-											</ul>
-											</div>
-										</div>
-										<div className="product-info-bottom">
-											<div className="product-price">
-											<span>$34,900<label>/Month</label></span>
-											</div>
-										</div>
-										</div>
-									</div> */}
+                    <div className="col-lg-12"></div>
+
                     <div className="ltn__product-slider-area ltn__product-gutter pt-40 pb-40">
                       <div className="container-fluid">
                         <div className="row ltn__product slick-arrow-1">
-                          {props?.propertyTypes?.map((categoryProperty) => (
+                          {paginatedPropertyTypes?.map((categoryProperty) => (
                             <div className="col-lg-4">
                               <div
                                 key={categoryProperty?.id}
@@ -363,6 +305,18 @@ const TyepPropertyGrid = (props) => {
 
                           {/*  */}
                         </div>
+                        <ReactPaginate
+                          previousLabel={"Previous"}
+                          nextLabel={"Next"}
+                          breakLabel={"..."}
+                          pageCount={pageCount}
+                          pageRangeDisplayed={5}
+                          onPageChange={handlePageChange}
+                          containerClassName={'pagination'}
+                          activeClassName={'pagination__link--active'}
+                          disabledClassName={'pagination__link--disabled'}
+                    
+                        />
                       </div>
                     </div>
 
